3f5c9094107fb41cc02b5e5d1ed0218cd394c867
krishna sain <sainkrishna510@gmail.com>
2026-05-08 20:31:26 +0530

    8-09-26

--- sum.test.js
+++ sum.test.js
@@ -8,7 +8,7 @@ test("adds 2 +2 is equal 4",()=>{
 test("adds -5 + -5 is equal -10",()=>{
     expect(sum(-5,-5)).toBe(-10)
 })
-test("adds -10 + -10 is equal -10",()=>{
-    expect(sum(-10,-10)).toBe(-20)
+test("adds -30 + -30 is equal -60",()=>{
+    expect(sum(-30,-30)).toBe(-60)
 })
 })
